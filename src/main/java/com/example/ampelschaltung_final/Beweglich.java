c32f9caf5fecb6de1f2162fece99ca4e9d8d952c
Programmpommes <frederik21@web.de>
2023-06-25 17:05:55 +0200

    Ampel mit Auto verknüpft

--- src/main/java/com/example/ampelschaltung_final/Beweglich.java
+++ src/main/java/com/example/ampelschaltung_final/Beweglich.java
@@ -1,4 +1,19 @@
 package com.example.ampelschaltung_final;
 
 public class Beweglich {
+    Ampel ampel1 = new Ampel();
+    public Boolean halt = false;
+
+
+    public void stopGo(){
+        if(ampel1.istRot1 || ampel1.istRot2){
+            halt = true;
+        }
+        else if (!ampel1.istRot1 || !ampel1.istRot2){
+            halt = false;
+        }
+    }
+
+
+
 }
